ab91fe951fd43079cd437cae86762125f3f761b8
Juan Ignacio Langdon Sagasta <nacholangdon@gmail.com>
2022-06-17 15:02:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -107,3 +107,10 @@ function t() {
     clearInterval(timer);
   }
 }
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+} else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
